d7394527205a53da32ae06e79d258ce8ead63840
Mads Alber Kuhlmann-Jørgensen <makuh17@student.sdu.dk>
2020-02-18 16:03:40 +0100

    slight changes to main

--- Baseline_method/main.cpp
+++ Baseline_method/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-#include "../Includes/brick_detector.h"
+#include "../Includes_restructured/Brick_Detector.h"
+#include "../Includes_restructured/Evaluator.h"
 
 using namespace std;
 
 int main()
 {
-    Brick_Detector detector("../Strojer_Images/Initial Test Images/Cropped");
-    detector.predict_all_images();
+    //Brick_Detector detector("../Strojer_Images/Initial Test Images/Cropped");
+    //detector.predict_all_images();
     cout << "Hello World!" << endl;
     return 0;
 }
