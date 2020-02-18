fdfa5d689adaa4dc97643c27ea594b01ac3b5258
Jakob Grøftehauge <jakra17@student.sdu.dk>
2020-02-18 20:29:55 +0100

    Brick_Detector compiles, and appears to be working

--- Baseline_method/main.cpp
+++ Baseline_method/main.cpp
@@ -6,9 +6,12 @@ using namespace std;
 
 int main()
 {
-    cv::Mat img;
+    cv::Mat img = cv::imread("/home/jakob/Documents/Bachelor Project/Strojer_Images/Initial Test Images/Cropped/IMG_4049.JPG", CV_LOAD_IMAGE_COLOR);
+    /*cv::imshow("test", img);
+    cv::waitKey(0);*/
     Brick_Detector test;
     test.set_img(img);
+    test.detect();
     //Brick_Detector detector("../Strojer_Images/Initial Test Images/Cropped");
     //detector.predict_all_images();
     cout << "Hello World!" << endl;
--- Includes_restructured/Brick_Detector.h
+++ Includes_restructured/Brick_Detector.h
@@ -27,11 +27,15 @@ private:
     void clear_all();
     void clear_predictions();
     void find_lines();
+    void find_BB(std::vector<std::vector<std::vector<double> > > clustered_lines);
     void find_edges(cv::Mat &src, cv::Mat &dst);
-    void find_BB();
+
     std::vector<std::vector<std::vector<double>>> cluster_lines();
     std::vector<std::vector<double>> sort_lines(std::vector<std::vector<double>> lines);
-    std::vector<std::vector<cv::Point2f>> find_intersection_matrix(std::vector<std::vector<std::vector<double>>> sorted_lines);
-
+    std::vector<std::vector<cv::Point2f>> find_intersection_matrix(std::vector<std::vector<std::vector<double>>> clustered_lines_sorted);
+    cv::Point2f find_intersection(std::vector<double> line1, std::vector<double> line2);
+    std::vector<cv::Point2f> get_points_on_line(std::vector<double> line);
+    void convert_intections_to_BB(std::vector<std::vector<cv::Point2f>> intersection_matrix);
+    bool accept_detection(cv::RotatedRect rotated_BB);
 };
 
