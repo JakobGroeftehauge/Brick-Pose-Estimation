83b9f3059ebe0e6fdda06f875cd16a40a732c1e0
Mads Alber Kuhlmann-Jørgensen <makuh17@student.sdu.dk>
2020-02-18 09:29:51 +0100

    created relative path

--- Baseline_method/main.cpp
+++ Baseline_method/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    Brick_Detector detector("/home/jakob/Documents/Bachelor Project/Strojer_Images/Initial Test Images/Cropped");
+    Brick_Detector detector("../Strojer_Images/Initial Test Images/Cropped");
     detector.predict_all_images();
     cout << "Hello World!" << endl;
     return 0;
