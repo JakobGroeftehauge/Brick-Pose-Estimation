ece49c8e995e249fb763c9a0befe94ad5d1b6a0c
Mads Alber Kuhlmann-Jørgensen <makuh17@student.sdu.dk>
2020-02-18 14:54:42 +0100

    renamed Evaluation to Evaluator

---
+++ Includes_restructured/Evaluator.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+#include "data_loader.h"
+#include "Brick_Detector.h"
+class Evaluator
+{
+public:
+	Evaluator();
+	Evaluator(std::string path);
+	void set_path(std::string path);
+	void predict_next_img();
+	void open_file(std::string file_name);
+	void close_file();
+
+private:
+	double calculate_IoU(cv::Rect rect1, cv::Rect rect2);
+	void print_results_to_csv();
+	std::string folder_path;
+	Data_loader loader;
+	std::ofstream file;
+};
+
