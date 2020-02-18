3784f0d19222f2ec2d23442b90adab2720559ec2
Jakob Grøftehauge <jakra17@student.sdu.dk>
2020-02-18 16:04:38 +0100

    Refactored annotation loader and data loader

--- Baseline_method/main.cpp
+++ Baseline_method/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
-#include "../Includes/brick_detector.h"
+#include "../Includes_restructured/Brick_Detector.h"
+//#include "Brick_Detector.h"
 
 using namespace std;
 
 int main()
 {
-    Brick_Detector detector("../Strojer_Images/Initial Test Images/Cropped");
-    detector.predict_all_images();
+    cv::Mat img;
+    Brick_Detector test;
+    test.set_img(img);
+    //Brick_Detector detector("../Strojer_Images/Initial Test Images/Cropped");
+    //detector.predict_all_images();
     cout << "Hello World!" << endl;
     return 0;
 }
--- Includes_restructured/Brick_Detector.h
+++ Includes_restructured/Brick_Detector.h
@@ -5,8 +5,10 @@ struct prediction {
 	cv::RotatedRect rotated_rect;
 	cv::Point2d angle_vector;
 };
+
 class Brick_Detector
 {
+
 public:
 	Brick_Detector();
 	Brick_Detector(cv::Mat img);
@@ -16,7 +18,20 @@ public:
 	std::vector<prediction> predictions;
 private:
 	cv::Mat img;
-	void clear();
+
+    int canny_thres_low;
+    int canny_thres_high;
+
+    std::vector<std::vector<double>> lines;
+
+    void clear_all();
+    void clear_predictions();
+    void find_lines();
+    void find_edges(cv::Mat &src, cv::Mat &dst);
+    void find_BB();
+    std::vector<std::vector<std::vector<double>>> cluster_lines();
+    std::vector<std::vector<double>> sort_lines(std::vector<std::vector<double>> lines);
+    std::vector<std::vector<cv::Point2f>> find_intersection_matrix(std::vector<std::vector<std::vector<double>>> sorted_lines);
 
 };
 
