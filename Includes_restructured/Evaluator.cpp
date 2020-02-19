5dfd9614274ab79de34aa51f81f2fd1d37bed488
Mads Alber Kuhlmann-Jørgensen <makuh17@student.sdu.dk>
2020-02-19 09:34:42 +0100

    evaluator updated

--- Includes_restructured/Evaluator.cpp
+++ Includes_restructured/Evaluator.cpp
@@ -14,7 +14,33 @@ void Evaluator::set_path(std::string path)
 	this->loader = Data_loader(path);
 }
 
+bool Evaluator::evaluate_next_img(double threshold = 0.5)
+{
+	if (loader.loadNext())
+	{
+		this->detector.detect(loader.img);
+
+		return true;
+	}
+	else
+	{
+		std::cout << "No more images to evaluate" << std::endl;
+		return false;
+	}
+}
+
 double Evaluator::calculate_IoU(cv::Rect rect1, cv::Rect rect2)
 {
 	return (rect1 & rect2).area() / (rect1 | rect2).area();
+}
+
+void Evaluator::evaluate(double threshold = 0.5)
+{
+	std::vector<cv::Rect> annotations(this->loader.Bounding_boxes);
+	std::vector<cv::Rect> predictions;
+	for (int i = 0; i < detector.predictions.size(); i++)
+	{
+		predictions.push_back(detector.predictions[i].rect);
+	}
+
 }
--- Includes_restructured/Evaluator.h
+++ Includes_restructured/Evaluator.h
@@ -1,20 +1,24 @@
 #pragma once
 #include <opencv2/opencv.hpp>
+#include <iostream>
 #include "data_loader.h"
 #include "Brick_Detector.h"
+#include "util.h"
 class Evaluator
 {
 public:
 	Evaluator();
 	Evaluator(std::string path);
 	void set_path(std::string path);
-	void evaluate_next_img();
+	bool evaluate_next_img(double threshold = 0.5);
 	void open_file(std::string file_name);
 	void close_file();
 
 private:
 	double calculate_IoU(cv::Rect rect1, cv::Rect rect2);
 	void print_results_to_csv();
+	void evaluate(double threshold = 0.5);
+	void save_evaluation(int true_pos, int false_pos, int false_neg);
 	//std::string folder_path;
 	Data_loader loader;
 	std::ofstream file;
