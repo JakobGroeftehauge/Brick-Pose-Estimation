5123c245531a3da323330449db56cf9b07230ffd
jakobGroeftehauge <jakra17@student.sdu.dk>
2020-03-15 12:34:05 +0100

    Generator only returning regression and classification batch

--- 02 Deep Learning/RetinaNet with Angle/keras_retinanet/preprocessing/generator.py
+++ 02 Deep Learning/RetinaNet with Angle/keras_retinanet/preprocessing/generator.py
@@ -363,6 +363,9 @@ class Generator(keras.utils.Sequence):
         # compute network targets
         targets = self.compute_targets(image_group, annotations_group)
 
+        regression_batch, labels_batch, angles_batch = targets
+        targets = (regression_batch, labels_batch)
+
         return inputs, targets
 
     def __len__(self):
