9d9fda63a097b5493dc7c2b8af13b1ea230df2dc
Mads Alber Kuhlmann-Jørgensen <makuh17@student.sdu.dk>
2020-03-15 13:22:48 +0100

    added initialisation of angle

--- 02 Deep Learning/RetinaNet with Angle/keras_retinanet/models/retinanet.py
+++ 02 Deep Learning/RetinaNet with Angle/keras_retinanet/models/retinanet.py
@@ -306,8 +306,9 @@ def retinanet(
         num_anchors = AnchorParameters.default.num_anchors()
 
     if submodels is None:
-        #submodels = default_submodels_and_angle(num_classes, num_anchors)
-        submodels = default_submodels(num_classes, num_anchors) #  original
+        submodels = default_submodels_and_angle(num_classes, num_anchors)
+        submodels = submodels[0:1]
+        #submodels = default_submodels(num_classes, num_anchors) #  original
 
     C3, C4, C5 = backbone_layers
 
