afc8b1c6e2a6aec7cab4ab5122f16c30dc41f1ae
Mads Alber Kuhlmann-Jørgensen <makuh17@student.sdu.dk>
2020-03-16 09:28:09 +0100

    remove sigmoid

--- 02 Deep Learning/RetinaNet with Angle/keras_retinanet/models/retinanet.py
+++ 02 Deep Learning/RetinaNet with Angle/keras_retinanet/models/retinanet.py
@@ -161,7 +161,7 @@ def angle_regression_model(num_values, num_anchors, pyramid_feature_size=256, re
             **options
         )(outputs)
 
-    outputs = keras.layers.Conv2D(num_anchors * num_values, activation = 'sigmoid', name='pyramid_regression', **options)(outputs)
+    outputs = keras.layers.Conv2D(num_anchors * num_values, name='pyramid_regression', **options)(outputs)
     if keras.backend.image_data_format() == 'channels_first':
         outputs = keras.layers.Permute((2, 3, 1), name='pyramid_regression_permute')(outputs)
     outputs = keras.layers.Reshape((-1, num_values), name='pyramid_regression_reshape')(outputs)
