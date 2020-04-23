1d8555e42cd1009e8c8ab5d4174a481d368d7313
Mads Alber Kuhlmann-Jørgensen <makuh17@student.sdu.dk>
2020-04-23 14:51:37 +0200

    chamfer_CCORR_NORM_0-0.1 evaluation finished

--- 01 Classic Vision/Chamfer_evaluation/Eval_Chamfer_Axis_Aligned.cpp
+++ 01 Classic Vision/Chamfer_evaluation/Eval_Chamfer_Axis_Aligned.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main()
 {
-	std::string test_name = "chamfer_CCORR_NORM_0-1";
+	std::string test_name = "chamfer_CCORR_NORM_0-0.1";
 	std::ofstream res, dump;
 
 	int sp = 12;
 	std::vector<double> thresholds({ 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95 });
-	std::vector<double> nms_thresh = {0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95};// , 0.2, 0.25, 0.3, 0.35, 0.4};
+	std::vector<double> nms_thresh = {0, 0.005, 0.01, 0.015, 0.02, 0.025, 0.03, 0.035, 0.04, 0.045, 0.05, 0.055, 0.06, 0.065, 0.07, 0.075, 0.08, 0.085, 0.09, 0.095};// , 0.2, 0.25, 0.3, 0.35, 0.4};
 	Chamfer_brick_detector detector; // hough based brick detector
 	Evaluator test_evaluator("../../03 Data/Simple Dataset", "../../02 Deep Learning/Create-CSV-dataset/list_of_img_in_val_set_18-03.csv");
 	test_evaluator.set_detector(&detector);
