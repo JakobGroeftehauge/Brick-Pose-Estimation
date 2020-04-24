8f736c0f4ca616edeae8869fd8bc6795ad783115
Mads Alber Kuhlmann-Jørgensen <makuh17@student.sdu.dk>
2020-04-24 14:42:38 +0200

    new evaluation file for deep learning

--- 01 Classic Vision/Chamfer_evaluation/Eval_Chamfer_Axis_Aligned.cpp
+++ 01 Classic Vision/Chamfer_evaluation/Eval_Chamfer_Axis_Aligned.cpp
@@ -5,20 +5,22 @@ using namespace std;
 
 int main()
 {
-	std::string test_name = "chamfer_OUR-CCORR_0-500";
+	std::string test_name = "chamfer_OUR-CCORR_Gauss3";
 	std::ofstream res, dump;
 
 	int sp = 12;
 	std::vector<double> thresholds({ 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95 });
-	std::vector<double> nms_thresh = { 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375, 400, 425, 450, 475, 500 };// , 0.2, 0.25, 0.3, 0.35, 0.4};
+	std::vector<double> nms_thresh = { 125 };// { 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375, 400, 425, 450, 475, 500 };// , 0.2, 0.25, 0.3, 0.35, 0.4};
 	Chamfer_brick_detector detector; // hough based brick detector
-	Evaluator test_evaluator("../../03 Data/Simple Dataset", "../../02 Deep Learning/Create-CSV-dataset/list_of_img_in_val_set_18-03.csv");
+	//Evaluator test_evaluator("../../03 Data/Simple Dataset", "../../02 Deep Learning/Create-CSV-dataset/list_of_img_in_val_set_18-03.csv");
+	Evaluator test_evaluator("../../03 Data/Dataset2_onPallet", "../../02 Deep Learning/Create-CSV-dataset/list_of_img_in_OP_val_set_14-04.csv");
 	test_evaluator.set_detector(&detector);
-	test_evaluator.detector->set_brick_specs(96, 21, 108, 28);
+	//test_evaluator.detector->set_brick_specs(96, 21, 108, 28);
+	test_evaluator.detector->set_brick_specs(139, 30, 211, 54.39);
 	test_evaluator.detector->set_resolution(60, 5);
 
-	res.open("../../03 Data/Simple Dataset/" + test_name + "results.csv");
-	dump.open("../../03 Data/Simple Dataset/" + test_name + "dump.txt");
+	res.open("../../03 Data/Dataset2_onPallet/" + test_name + "results.csv");
+	dump.open("../../03 Data/Dataset2_onPallet/" + test_name + "dump.txt");
 	res << setw(sp) << "Thresh, Prec., Rec., F1, ang. err., a. e. std., Time" << std::endl;
 
 	for (int i = 0; i < nms_thresh.size(); i++)
