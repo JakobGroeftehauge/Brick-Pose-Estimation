e70ebb33a97ea795bc7b0049e14fbff098e62aca
Mads Alber Kuhlmann-Jørgensen <makuh17@student.sdu.dk>
2020-04-30 14:13:29 +0200

    Update anchors.py

--- 02 Deep Learning/RetinaNet_Rotated_Comb/keras_retinanet/utils/anchors.py
+++ 02 Deep Learning/RetinaNet_Rotated_Comb/keras_retinanet/utils/anchors.py
@@ -135,6 +135,11 @@ def compute_center_diffs_opt(anchors, annotations):
     anch_center_y = (anchors[:, 1] + anchors[:, 3]) * 0.5
     anno_height = np.abs((annotations[:, 1] - annotations[: , 3]))
 
+    anno_center_x_mat = np.tile(anno_center_x.T, (anch_center_x.shape[0], 1))
+    anch_center_x_mat = np.tile(anch_center_x, (1,anno_center_x.shape[0]))
+    print("anno center x shape: ", anno_center_x_mat.shape)
+    print("anch center x shape: ", anch_center_x_mat.shape)
+
     center_diffs_x = np.abs(np.tile(anno_center_x.T, (anch_center_x.shape[0], 1)) - np.tile(anch_center_x, (1,anno_center_x.shape[0])))
     center_diffs_y = np.abs(np.tile(anno_center_y.T, (anch_center_y.shape[0], 1)) - np.tile(anch_center_y, (1,anno_center_y.shape[0])))
 
