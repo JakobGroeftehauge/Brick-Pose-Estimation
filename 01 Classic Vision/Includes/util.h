8997c3c4973011596a381b1cded2df0d09ca7f18
jakobGroeftehauge <jakra17@student.sdu.dk>
2020-05-26 15:33:08 +0200

    Created figures

--- 01 Classic Vision/Includes/util.h
+++ 01 Classic Vision/Includes/util.h
@@ -76,7 +76,7 @@ inline void print_lines(cv::Mat &img, std::vector<std::vector<double>> lines, cv
     }
 }
 
-inline void print_rotated_bounding_boxes(cv::Mat &img, std::vector<bounding_box> preds, cv::Scalar color = cv::Scalar(0, 0, 255))
+inline void print_rotated_bounding_boxes(cv::Mat &img, std::vector<bounding_box> preds, cv::Scalar color = cv::Scalar(0, 0, 255), int line_width = 1)
 {
     for(unsigned int  i = 0; i < preds.size(); i++)
     {
@@ -85,7 +85,7 @@ inline void print_rotated_bounding_boxes(cv::Mat &img, std::vector<bounding_box>
 
         for(int j = 0; j < 4; j++)
         {
-            cv::line(img, rect_points[j], rect_points[(j + 1) % 4], color, 1, 8);
+            cv::line(img, rect_points[j], rect_points[(j + 1) % 4], color, line_width, 8);
         }
     }
 }
