34baab8b8cd68ebf89947dca11d25e00d92e00cb
Mads Alber Kuhlmann-Jørgensen <makuh17@student.sdu.dk>
2020-05-28 12:48:20 +0200

    classic vision, removed miscellaneous projects and projects for generating report figures

--- 04 Numerical Methods/Data/figures.m
+++
@@ -1,7 +0,0 @@
-h = figure;
-plot(d1_mat(:,1:2), '.');
-legend('\theta_1','\theta_2','x','y')
-set(h,'Units','Inches');
-pos = get(h,'Position');
-set(h,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
-print(h,'d1_angle','-dpdf','-r0')
